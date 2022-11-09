67f33ce13691642c8c17493380f1c7a495f35087
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2022-11-09 12:17:28 +0100

    badge

--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 #  VS Code + TypeScript + Node.js + Jest + GitHub + Heroku
+badge:
+[![Badge](https://img.shields.io/github/package-json/dependency-version/14A-Hegyi-Aron/JedlikTsTemplate/dev/@types/jest?color=%2315c213&label=jest&style=for-the-badge)](https://img.shields.io/github/package-json/dependency-version/14A-Hegyi-Aron/JedlikTsTemplate/dev/@types/jest?color=%2315c213&label=jest&style=for-the-badge)
 
 ## Cyclic teszt:
 https://jedlik-ts-template.cyclic.app/
---
+++ src/__tests__/Megoldás.test.ts
@@ -0,0 +1,27 @@
+import Megoldás from "../megoldás";
+
+describe("Megoldás osztály unit tesztek", () => {
+    const instance: Megoldás = new Megoldás("utasadat.txt");
+
+    it("Megoldás osztálypéldány ellenörzése", () => {
+        expect(instance).toBeInstanceOf(Megoldás);
+    });
+
+    it("Felszállók száma", () => {
+        expect(instance.felszállókSzáma).toBe(699);
+    });
+
+    it("Érvénytelen felszállások száma", () => {
+        expect(instance.érvénytelenFelszállásokSzáma).toBe(21);
+    });
+
+    it("Maximum keresés Array-el", () => {
+        expect(instance.MaxKeresArray.maxFelszálló).toBe(39);
+        expect(instance.MaxKeresArray.maxElsőMegálló).toBe(8);
+    });
+
+    it("Maximum keresés Map-el", () => {
+        expect(instance.MaxKeresMap.maxFelszálló).toBe(39);
+        expect(instance.MaxKeresMap.maxElsőMegálló).toBe(8);
+    });
+});
